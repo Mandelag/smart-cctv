1fc8fc03c6774c949eb9ee337db5fa746808097b
Keenan Gebze <keenan.gebze@gmail.com>
2018-04-28 17:22:57 +0700

    Remove unused code in FXHelloCVController

--- src/com/mandelag/smartcctv/FXHelloCVController.java
+++ src/com/mandelag/smartcctv/FXHelloCVController.java
@@ -49,9 +49,6 @@ public class FXHelloCVController implements Initializable {
     @FXML
     private ImageView currentFrame;
 
-    // a timer for acquiring the video stream
-    private ScheduledExecutorService timer;
-    // a flag to change the button behavior
     // the id of the camera to be used
     private static int cameraId = 0;
 
@@ -74,89 +71,69 @@ public class FXHelloCVController implements Initializable {
     @FXML
     protected void startCamera() {
         Runnable frameGrabber = new Runnable() {
-
             @Override
             public void run() {
-                try {
-                    URL url = new URL(cctvUrl);
-                    URLConnection huc = url.openConnection();
+                while (!Thread.interrupted()) {
+                    try {
+                        URL url = new URL(cctvUrl);
+                        URLConnection huc = url.openConnection();
 
-                    try (InputStream is = new BufferedInputStream(huc.getInputStream()); BufferedReader rs = new BufferedReader(new InputStreamReader(is))) {
-                        int[] magicInt = {0xff, 0xd8, 0xff};
-                        byte[] magicIntByte = new byte[magicInt.length];
-                        for (int i = 0; i < magicInt.length; i++) {
-                            magicIntByte[i] = (byte) magicInt[i];
-                        }
-                        Consumer<byte[]> byteConsumer = (b) -> {
-                            try (ByteArrayOutputStream bos = new ByteArrayOutputStream()) {
-                                bos.write(magicIntByte);
-                                bos.write(b);
-                                bos.flush();
-
-                                byte[] imageBytes = bos.toByteArray();
-
-                                Mat frame = Imgcodecs.imdecode(new MatOfByte(imageBytes), Imgcodecs.CV_LOAD_IMAGE_UNCHANGED);//CV_LOAD_IMAGE_UNCHANGED
-                                //Mat rot = Imgproc.getRotationMatrix2D(new Point(frame.width()/2, frame.height()/2), 21, 1);
-                                //Imgproc.warpAffine(frame, frame, rot, new Size(frame.width(), frame.height()));
-                                MatOfRect matOfRect = new MatOfRect();
-                                carsClassifier.detectMultiScale(frame, matOfRect, 1.4, 0, 0, new Size(30, 30), new Size(65, 65));
-                                //System.out.print(matOfRect.dump());
-                                Rect[] t = matOfRect.toArray();
-                                if (t.length > 0) {
-                                    System.out.println(t.length + " car(s) detected.");
-                                    for (int i = 0; i < t.length; i++) {
-                                        Imgproc.rectangle(frame, new Point(t[i].x, t[i].y),
-                                                new Point(t[i].x + t[i].width - 1, t[i].y + t[i].height - 1),
-                                                new Scalar(255, 255, 0));
-                                    }
-                                }
-                                Image imageToShow = Utils.mat2Image(frame);
-                                updateImageView(currentFrame, imageToShow);
-
-                            } catch (IOException e) {
-                                System.err.println(e);
+                        try (InputStream is = new BufferedInputStream(huc.getInputStream()); BufferedReader rs = new BufferedReader(new InputStreamReader(is))) {
+                            int[] magicInt = {0xff, 0xd8, 0xff};
+                            byte[] magicIntByte = new byte[magicInt.length];
+                            for (int i = 0; i < magicInt.length; i++) {
+                                magicIntByte[i] = (byte) magicInt[i];
                             }
-                        };
-                        String separator = huc.getHeaderField("content-type").split("boundary=")[1];
-                        byte[] close = separator.getBytes();
-                        int[] intArray = new int[close.length];
-                        for (int i = 0; i < close.length; i++) {
-                            intArray[i] = (int) close[i];
-                        }
-                        InputStreamMarker imageExtract = new InputStreamMarker(magicInt, intArray, byteConsumer);
+                            Consumer<byte[]> byteConsumer = (b) -> {
+                                try (ByteArrayOutputStream bos = new ByteArrayOutputStream()) {
+                                    bos.write(magicIntByte);
+                                    bos.write(b);
+                                    bos.flush();
 
-                        InputStreamExtractor ise = new InputStreamExtractor(is, new InputStreamMarker[]{imageExtract});
-                        ise.extract();
+                                    byte[] imageBytes = bos.toByteArray();
+
+                                    Mat frame = Imgcodecs.imdecode(new MatOfByte(imageBytes), Imgcodecs.CV_LOAD_IMAGE_UNCHANGED);//CV_LOAD_IMAGE_UNCHANGED
+                                    //Mat rot = Imgproc.getRotationMatrix2D(new Point(frame.width()/2, frame.height()/2), 21, 1);
+                                    //Imgproc.warpAffine(frame, frame, rot, new Size(frame.width(), frame.height()));
+                                    MatOfRect matOfRect = new MatOfRect();
+                                    carsClassifier.detectMultiScale(frame, matOfRect, 1.4, 0, 0, new Size(30, 30), new Size(65, 65));
+                                    //System.out.print(matOfRect.dump());
+                                    Rect[] t = matOfRect.toArray();
+                                    if (t.length > 0) {
+                                        System.out.println(t.length + " car(s) detected.");
+                                        for (int i = 0; i < t.length; i++) {
+                                            Imgproc.rectangle(frame, new Point(t[i].x, t[i].y),
+                                                    new Point(t[i].x + t[i].width - 1, t[i].y + t[i].height - 1),
+                                                    new Scalar(255, 255, 0));
+                                        }
+                                    }
+                                    Image imageToShow = Utils.mat2Image(frame);
+                                    updateImageView(currentFrame, imageToShow);
+
+                                } catch (IOException e) {
+                                    System.err.println(e);
+                                }
+                            };
+                            String separator = huc.getHeaderField("content-type").split("boundary=")[1];
+                            byte[] close = separator.getBytes();
+                            int[] intArray = new int[close.length];
+                            for (int i = 0; i < close.length; i++) {
+                                intArray[i] = (int) close[i];
+                            }
+                            InputStreamMarker imageExtract = new InputStreamMarker(magicInt, intArray, byteConsumer);
+
+                            InputStreamExtractor ise = new InputStreamExtractor(is, new InputStreamMarker[]{imageExtract});
+                            ise.extract();
+                        } catch (IOException e) {
+                            System.err.println(e);
+                        }
                     } catch (IOException e) {
                         System.err.println(e);
                     }
-                } catch (IOException e) {
-                    System.err.println(e);
                 }
             }
-
         };
-
-        this.timer = Executors.newSingleThreadScheduledExecutor();
-        this.timer.scheduleAtFixedRate(frameGrabber, 0, 33, TimeUnit.MILLISECONDS);
-
-        // update the button content
-    }
-
-    /**
-     * Stop the acquisition from the camera and release all the resources
-     */
-    private void stopAcquisition() {
-        if (this.timer != null && !this.timer.isShutdown()) {
-            try {
-                // stop the timer
-                this.timer.shutdown();
-                this.timer.awaitTermination(33, TimeUnit.MILLISECONDS);
-            } catch (InterruptedException e) {
-                // log any exception
-                System.err.println("Exception in stopping the frame capture, trying to release the camera now... " + e);
-            }
-        }
+        new Thread(frameGrabber).start();
     }
 
     /**
@@ -173,7 +150,8 @@ public class FXHelloCVController implements Initializable {
      * On application close, stop the acquisition from the camera
      */
     protected void setClosed() {
-        this.stopAcquisition();
+        
+        System.exit(0);
     }
 
     @Override

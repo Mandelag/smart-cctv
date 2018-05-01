fb57b6504874be7ede3e23d768533f8096bcd343
Keenan Gebze <keenan.gebze@gmail.com>
2018-05-01 20:27:46 +0700

    Add functionality to process the object detection.

--- src/com/mandelag/smartcctv/services/MainCCTVService.java
+++ src/com/mandelag/smartcctv/services/MainCCTVService.java
@@ -10,6 +10,7 @@ import com.mandelag.extractor.InputStreamMarker;
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -29,14 +30,25 @@ import org.eclipse.jetty.server.handler.HandlerList;
 import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfByte;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
 
 /**
  *
  * @author Keenan
  */
 public class MainCCTVService {
+
     public static void main(String[] args) throws Exception {
-        
+
         //if(args.length < 3 ) {
         //    System.out.println("    Usage: java -jar com.mandelag.smartcctv.MainCCTVService <ip> <web_port> <cctv_address>");
         //    return;
@@ -44,12 +56,11 @@ public class MainCCTVService {
         String serverAddress = "localhost";
         String port = "9905";
         String cctv = "http://114.110.17.6:8896/image.jpg?type=motion";
-        
+
         Server server = new Server(new InetSocketAddress(InetAddress.getByName(serverAddress), Integer.parseInt(port)));
         URL cctvUrl = new URL(cctv);
         URLConnection huc = cctvUrl.openConnection();
-        
-        
+
         ServletContextHandler context = new ServletContextHandler();
         context.setContextPath("/");
         //context.addServlet("com.mandelag.smartcctv.services.CCTVServlet", "/smartcctv");
@@ -58,23 +69,32 @@ public class MainCCTVService {
         HandlerList handlers = new HandlerList();
         handlers.setHandlers(new Handler[]{context, new DefaultHandler()});
         server.setHandler(handlers);
-        
-        
-        int[] preImg = new int[]{0xff,0xd8,0xff};
+
+        int[] preImg = new int[]{0xff, 0xd8, 0xff};
         byte[] preImgByte = new byte[preImg.length];
-        for(int i=0; i < preImg.length ; i++) {
-                preImgByte[i] = (byte) preImg[i];
+        for (int i = 0; i < preImg.length; i++) {
+            preImgByte[i] = (byte) preImg[i];
         }
-        
-        try(InputStream is = new BufferedInputStream(huc.getInputStream()); BufferedReader rs = new BufferedReader(new InputStreamReader(is))) {
+
+        try (InputStream is = new BufferedInputStream(huc.getInputStream()); BufferedReader rs = new BufferedReader(new InputStreamReader(is))) {
             Consumer<byte[]> byteConsumer = (b) -> {
                 System.out.println("got byte!");
-                try(BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream("C:\\Users\\keenan\\extract.jpeg"))){
+                try (BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream("C:\\Users\\keenan\\extract.jpeg"))) {
                     bos.write(preImgByte);
                     bos.write(b);
-                    
-                }catch(IOException e){
+
+                } catch (IOException e) {
                     System.err.println(e);
+                }
+            };
+
+            Consumer<byte[]> imageProcessor = (b) -> {
+                byte[] buf = new byte[b.length + 3];
+                try (ByteArrayOutputStream bos = new ByteArrayOutputStream(b.length + 3)) {
+                    bos.write(preImgByte);
+                    bos.write(b);
+                    byte[] processedImage = bos.toByteArray();
+                } catch (IOException e) {
                 }
             };
             String separator = huc.getHeaderField("content-type").split("boundary=")[1];
@@ -82,27 +102,53 @@ public class MainCCTVService {
             System.out.println(separator);
             byte[] close = separator.getBytes();//Charset.forName("UTF-8")
             int[] intArray = new int[close.length];
-            for(int i=0; i < close.length ; i++) {
+            for (int i = 0; i < close.length; i++) {
                 intArray[i] = (int) close[i];
             }
-            
-            
+
             InputStreamMarker imageExtract = new InputStreamMarker(preImg, intArray, cs::receiveImage);
-            
+
             InputStreamExtractor ise = new InputStreamExtractor(is, new InputStreamMarker[]{imageExtract});
             new Thread(() -> {
                 try {
                     ise.extract();
-                }catch(IOException e) {
-                    
+                } catch (IOException e) {
+
                 }
             }).start();
-            
+
             server.start();
             server.join();
-            
-        }catch(IOException e){
+
+        } catch (IOException e) {
             System.err.println(e);
         }
+    }
+
+    private static CascadeClassifier carsClassifier = new CascadeClassifier();
+
+    static {
+        carsClassifier.load("C:\\Users\\keenan\\Documents\\NetBeansProjects\\JavaFXApplication2\\src\\res\\cars.xml");
+    }
+    
+    private byte[] processImage(byte[] imageBytes) {
+        Mat frame = Imgcodecs.imdecode(new MatOfByte(imageBytes), Imgcodecs.CV_LOAD_IMAGE_UNCHANGED);//CV_LOAD_IMAGE_UNCHANGED
+        //Mat rot = Imgproc.getRotationMatrix2D(new Point(frame.width()/2, frame.height()/2), 21, 1);
+        //Imgproc.warpAffine(frame, frame, rot, new Size(frame.width(), frame.height()));
+        MatOfRect matOfRect = new MatOfRect();
+        carsClassifier.detectMultiScale(frame, matOfRect, 1.4, 0, 0, new Size(30, 30), new Size(155, 155));
+        //System.out.print(matOfRect.dump());
+        Rect[] t = matOfRect.toArray();
+        if (t.length > 0) {
+            System.out.println(t.length + " car(s) detected.");
+            for (int i = 0; i < t.length; i++) {
+                Imgproc.rectangle(frame, new Point(t[i].x, t[i].y),
+                        new Point(t[i].x + t[i].width - 1, t[i].y + t[i].height - 1),
+                        new Scalar(255, 255, 0));
+            }
+        }
+        MatOfByte matOfByte = new MatOfByte();
+        Imgcodecs.imencode(".JPEG", frame, matOfByte);
+        return matOfByte.toArray();
     }
 }
